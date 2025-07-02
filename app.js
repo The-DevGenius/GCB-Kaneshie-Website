f0011c1b2c417e70242f4ad017dd066b87c46ebf
Obuobi Ayim David <102563270+Aristocratjnr@users.noreply.github.com>
2025-07-02 11:09:14 +0000

    Update app.js

--- app.js
+++ app.js
@@ -9,13 +9,9 @@ app.use(bodyParser.json());
 
 // Route to handle the POST request from the frontend
 app.post('/create-account', (req, res) => {
-    // Assuming you have a database connection and an "accounts" collection to store customer data
-    // Save the customer data received from the frontend into the database
-
-    // For demonstration purposes, let's just log the received data and send a response
     console.log('Received customer data:', req.body);
     const response = {
-        insertedId: 'some_generated_id' // Replace with the actual inserted ID from the database
+        insertedId: 'some_generated_id' 
     };
     res.json(response);
 });
