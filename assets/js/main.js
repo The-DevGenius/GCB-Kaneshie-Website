3afc17a0c7caff7d5997ad4fda775f23be84cabb
Obuobi Ayim David <102563270+Aristocratjnr@users.noreply.github.com>
2025-07-02 11:23:21 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,12 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   /**
    * Preloader
    */
-  const preloader = document.querySelector('#preloader');
-  if (preloader) {
-    window.addEventListener('load', () => {
-      preloader.remove();
-    });
-  }
+ 
 
   /**
    * Mobile nav toggle
